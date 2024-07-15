cea245326dfefc48ba29d3d00bb043955018011e
ahmer ayaz <139497541+ahmerayaz2000@users.noreply.github.com>
2024-07-15 23:23:57 +0500

    Add files via upload

---
+++ airthmaticoperations.py
@@ -0,0 +1,29 @@
+
+
+
+
+def mult(x, y):
+    print(x * y)
+
+# Call the function
+mult(2, 4)
+
+
+def div(a, b):
+    print(a / b)
+
+# Call the function outside of its definition
+div(4, 2)
+
+
+def add(x, y):
+    print(x + y)
+
+add(2, 2)    
+
+def diff(a , b):
+    print(a - b)
+
+diff(4 , 2)
+
+
